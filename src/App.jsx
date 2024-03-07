cea32df5256795f5522eaca5e2f20497e27daa86
Julian <jjvll90@gmail.com>
2024-03-06 21:43:08 -0600

    Add basic structure to App component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // You can create an App.css file for custom styles
+
+function App() {
+  return (
+    <div className="container my-5">
+      <h1 className="text-center">My Resume</h1>
+      {/* Add your resume content here */}
+    </div>
+  );
+}
+
+export default App;
